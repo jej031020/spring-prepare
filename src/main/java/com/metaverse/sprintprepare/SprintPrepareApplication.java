d3170fb9d5b720010a59b2d103d4d783ba9fb766
jej031020 <jej031020@naver.com>
2025-07-02 11:33:13 +0900

    chore: First Init

---
+++ src/main/java/com/metaverse/sprintprepare/SprintPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.sprintprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintPrepareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintPrepareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metaverse/sprintprepare/SprintPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.sprintprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
