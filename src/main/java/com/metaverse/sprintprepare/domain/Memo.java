f8264e8ea8c4ab3dd8e7c98cd6a367f1d099931c
jej031020 <jej031020@naver.com>
2025-07-03 15:51:12 +0900

    feat: Lombok 애너테이션 활용
    * @Getter를 통한 필드의 Get Method 자동 생성
    * @Setter를 통한 필드의 Set Method 자동 생성
    * Build 시(컴파일 시) 해당 메서드가 자동 구현됨

---
+++ src/main/java/com/metaverse/sprintprepare/domain/Memo.java
@@ -0,0 +1,12 @@
+package com.metaverse.sprintprepare.domain;
+
+import lombok.Getter;
+
+@Getter
+// @Setter // 값 변경
+public class Memo {
+    private String username;
+    private String content;
+    private String description;
+    private String password;
+}
