e13459862b458468378478368956cfa201bdfd7d
jej031020 <jej031020@naver.com>
2025-07-04 10:36:47 +0900

    feat: Lombok 애너테이션 활용 3
    * @RequiredArgsConstructor final 제어자가 붙은 필드를 가져야 하는 생성자

--- src/main/java/com/metaverse/sprintprepare/domain/Memo.java
+++ src/main/java/com/metaverse/sprintprepare/domain/Memo.java
@@ -1,17 +1,15 @@
 package com.metaverse.sprintprepare.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 //@Getter
 //@Setter // 값 변경
-@NoArgsConstructor // 기본 생성자 생성 애너테이션
-@AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자
+//@NoArgsConstructor // 기본 생성자 생성 애너테이션
+//@AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자1
+@RequiredArgsConstructor // final 필드를 필수로 가져야 하는 생성자
 public class Memo {
-    private String username;
+    private final String username; //private final: 필수로 있어야 하는 것
     private String content;
     private String description;
-    private String password;
+    private final String password;
 }
